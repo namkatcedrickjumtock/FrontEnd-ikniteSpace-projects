9323c47e6861744b3a275ea5f428021be8518f7e
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-15 08:27:05 +0100

    addedc comments

--- FCC projects/Javascript Data Structures and Algorithms/projects/palmdrone/palmdrone/palmdrone.js
+++ FCC projects/Javascript Data Structures and Algorithms/projects/palmdrone/palmdrone/palmdrone.js
@@ -1,4 +1,6 @@
 function palindrome(str) {
+
+  // remove non letters
     var re = /[\W_]/g;
     var lowRegStr = str.toLowerCase().replace(re, '');
     var reverseStr = lowRegStr.split('').reverse().join(''); 
